1430c3958ce0f4287d40875f4f9f12f739365f93
shubham98 <shubhambhojane90@gmail.com>
2021-01-14 12:00:53 +0530

    deployed the app

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 // import serviceWorkerRegistration from './serviceWorkerRegistration'
 import React from "react";
 import ReactDOM from "react-dom";
-import {HashRouter} from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from "./App";
 
 ReactDOM.render(<HashRouter><App /></HashRouter>, document.getElementById("root"));
